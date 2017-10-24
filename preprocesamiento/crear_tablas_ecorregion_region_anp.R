658ae39199751a7316cbea392a0f7cde13f9ac55
tereom <teresa.orti.mancera@gmail.com>
2017-10-24 13:08:28 -0500

    limpiar script

--- preprocesamiento/crear_tablas_ecorregion_region_anp.R
+++ preprocesamiento/crear_tablas_ecorregion_region_anp.R
@@ -13,7 +13,12 @@ library(maptools)
 setwd("/LUSTRE/sacmod/reportes_anp/procesar_ecorregion")
 
 # lista de archivos shapes
-path_anps_shp <- "/LUSTRE/MADMEX/eodata/footprints/anp/anp_sinBuffer"
+# path_anps_shp <- "/LUSTRE/MADMEX/eodata/footprints/anp/anp_sinBuffer"
+
+path_anps_shp <- "../datos_insumo/shapes_anp/anp_sinBuffer"
+# para anillos
+# path_anps_shp <- "../datos_insumo/shapes_anp/anp_rings"
+
 anps_shp <- list.files(path_anps_shp, 
     pattern = ".shp", recursive = FALSE) %>% 
     tools::file_path_sans_ext()
@@ -46,6 +51,7 @@ calcular_ecorregion <- function(anp_shp, path_anps_shp){
     anp_eco_df
 }
 
+# SLURM
 # data frame para iterar
 params_df <- dplyr::data_frame(anp_shp = anps_shp, path_anps_shp = path_anps_shp)
 sjob <- slurm_apply(calcular_ecorregion, params = params_df, jobname = "ecorregion_area_job", 
@@ -53,6 +59,7 @@ sjob <- slurm_apply(calcular_ecorregion, params = params_df, jobname = "ecorregi
         nodes = "5", ntasks = "5"), add_objects = "ecorregiones_7")
 print_job_status(sjob)
 
+# sin SLURM
 # anp_eco_list <- purrr::map(anps_shp, ~calcular_ecorregion(., path_anps_shp))
 
 ### Read results
@@ -66,78 +73,59 @@ anp_eco_list <- list.files(".", "results",
     map(readRDS) %>% 
     flatten() 
 
-anp_eco_df <- bind_rows(anp_eco_list) %>% 
-    mutate(anp = stringr::str_replace(anp, "anp_terrestres_2017_NOMBRE_", ""))
+anp_eco_df <- bind_rows(anp_eco_list) 
+save(anp_eco_df, file = "../../datos_procesados/2017-10-20_ecorregion.RData")
 
-save(anp_eco_df, file = "../../datos_procesados/2017-10-07_ecorregion.RData")
+# anillos
+anp_rings_eco_df <- bind_rows(anp_eco_list) 
+save(anp_rings_eco_df, file = "../datos_procesados/2017-10-20_ecorregion_rings.RData")
 
-
-### correr en LUSTRE ANILLOS
-library(raster)
-library(rslurm)
-library(rgdal)
-library(sp)
-library(rgeos)
-library(dplyr)
-library(stringr)
-library(purrr)
-library(maptools)
-
-# para correr en LUSTRE ponemos directorio de trabajo
-setwd("/LUSTRE/sacmod/reportes_anp/procesar_ecorregion_anillos")
-
-path_anps_shp <- "/LUSTRE/MADMEX/eodata/footprints/anp/anp_rings"
-anps_shp <- list.files(path_anps_shp,
-    pattern = ".shp", recursive = FALSE) %>%
-    tools::file_path_sans_ext()
-
-### correr función y crear ecorregiones_7
-
-params_df <- dplyr::data_frame(anp_shp = anps_shp, path_anps_shp = path_anps_shp)
-sjob <- slurm_apply(calcular_ecorregion, params = params_df, jobname = "ecorregion_area_job",
-nodes = 5, cpus_per_node = 2, slurm_options = list(partition = "optimus",
-nodes = "5", ntasks = "5"), add_objects = "ecorregiones_7")
-print_job_status(sjob)
-
-# anp_eco_list <- purrr::map(anps_shp, ~calcular_ecorregion(., path_anps_shp))
-
-### Read results
-library(dplyr)
-library(purrr)
-
-setwd("/LUSTRE/sacmod/reportes_anp/procesar_ecorregion_anillos/_rslurm_ecorregion_area_job")
-
-anp_eco_list <- list.files(".", "results",
-    full.names = TRUE) %>%
-    map(readRDS) %>%
-    flatten()
-
-anp_rings_eco_df <- bind_rows(anp_eco_list) %>%
-    mutate(anp = stringr::str_replace(anp, "anp_terrestres_2017_NOMBRE_", ""))
-
-save(anp_rings_eco_df, file = "../../datos_procesados/2017-10-07_ecorregion_rings.RData")
 
 ##############################################################################
 # crear tabla región CONANP
 ##############################################################################
+setwd("/LUSTRE/sacmod/reportes_anp")
 
 calcular_region <- function(anp_shp, path_anps_shp){
     anp <- readOGR(path_anps_shp, anp_shp, verbose = FALSE)
-    distinct(data_frame(region = anp$REGION, anp = anp_shp))
+    distinct(data_frame(region = as.character(anp$REGION), anp = anp_shp))
 }
 
 # lista de archivos shapes
-path_anps_shp <- "/LUSTRE/MADMEX/eodata/footprints/anp/anp_sinBuffer"
+# path_anps_shp <- "/LUSTRE/MADMEX/eodata/footprints/anp/anp_sinBuffer"
+
+path_anps_shp <- "datos_insumo/shapes_anp/anp_sinBuffer"
 anps_shp <- list.files(path_anps_shp, 
     pattern = ".shp", recursive = FALSE) %>% 
     tools::file_path_sans_ext()
 
 anp_region <- purrr::map_df(anps_shp, ~calcular_region(., path_anps_shp))
+
+# anp's que pertenecen a más de una región CONANP
+anp_region %>% 
+    filter(stringr::str_detect(region, ";")) %>% 
+    pull(anp)
+
+# eliminando los que pertenecen a más de una región
 anp_region_cl <- anp_region %>% 
     mutate(
-        region = str_replace(region, "Dirección Regional ", ""), 
-        anp = str_replace(anp, "anp_terrestres_2017_NOMBRE_", "") 
+        region = str_replace(region, "Dirección Regional ", "") # , 
+        # anp = str_replace(anp, "anp_terrestres_2017_NOMBRE_", "") 
     ) %>% 
     filter(!(stringr::str_detect(region, ";")))
 
-save(anp_region_cl, file = "../datos_procesados/2017-10-18_anp_region.RData")
+save(anp_region_cl, file = "../datos_procesados/2017-10-23_anp_region.RData")
+
+
+# pendiente lo de abajo
+anp_region_cl_reps <- data_frame(
+    region = c("Noreste y Sierra Madre Oriental", "Occidente y Pacífico Centro", 
+        "Península de Baja California y Pacífico Norte", "Noroeste y Alto Golfo de California"), 
+    anp = c("anp_terrestres_2017_NOMBRE_C.A.D.N.R._043_Estado_de_Nayarit", 
+        "anp_terrestres_2017_NOMBRE_C.A.D.N.R._043_Estado_de_Nayarit", 
+        "anp_terrestres_2017_NOMBRE_Islas_del_Golfo_de_California", 
+        "anp_terrestres_2017_NOMBRE_Islas_del_Golfo_de_California"))
+
+anp_region_cl <- bind_rows(anp_region_cl, anp_region_cl_reps)
+
+
