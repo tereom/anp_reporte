18b16efdb9fb3d82b03fb5c6e75c48d3a1b94511
tereom <teresa.orti.mancera@gmail.com>
2017-10-24 13:07:17 -0500

    limpiar script

--- preprocesamiento/crear_datos_cobertura_slurm.R
+++ preprocesamiento/crear_datos_cobertura_slurm.R
@@ -11,92 +11,60 @@ library(lubridate)
 
 setwd("/LUSTRE/sacmod/reportes_anp/procesar_forest_loss")
 
-path_anps_shp <- "/LUSTRE/MADMEX/eodata/footprints/anp/anp_sinBuffer"
+# anps
+path_anps_shp <- "/LUSTRE/sacmod/reportes_anp/datos_insumo/shapes_anp/anp_sinBuffer"
+#anillos
+path_anps_shp <- "/LUSTRE/sacmod/reportes_anp/datos_insumo/shapes_anp/anp_rings"
 
 anps_shp <- list.files(path_anps_shp, 
     pattern = ".shp", recursive = FALSE) %>% 
     tools::file_path_sans_ext()
 
-hansen_loss <- raster("/LUSTRE/sacmod/reportes_anp/datos/hansen_forest_loss_v1_3.tif")
-madmex_class <- raster("/LUSTRE/sacmod/reportes_anp/datos_procesados/madmex_nalc_10c_30m_2010.tif")
+hansen_loss <- raster::raster("../datos_insumo/hansen_forest_loss_v1_4_wgs84nodefs.tif")
+madmex_class <- raster::raster("../datos_insumo/madmex_nalc_10c_30m_2010.tif")
 
 perdida <- function(anp_shp, path_anps_shp){
-    anp  <- readOGR(path_anps_shp, anp_shp)
+    anp  <- rgdal::readOGR(path_anps_shp, anp_shp)
     anp_loss <- hansen_loss %>% 
-        crop(anp) %>%            # cortamos el raster a la dimensión del ANP
-        mask(mask = anp)         # ponemos máscara ANP
+        raster::crop(anp) %>%            # cortamos el raster a la dimensión del ANP
+        raster::mask(mask = anp)         # ponemos máscara ANP
     anp_madmex <- madmex_class %>% 
-        crop(anp) %>%            # cortamos el raster a la dimensión del ANP
-        mask(mask = anp) %>%     # ponemos máscara ANP
-        projectRaster(anp_loss, method = "ngb") # reproyectamos para que sean iguales
-    
+        raster::crop(anp) %>%            # cortamos el raster a la dimensión del ANP
+        raster::mask(mask = anp) %>% 
+        raster::projectRaster(anp_loss, method = "ngb")
+
     # stack rasters
-    anp_madmex_loss <- stack(list(anp_loss, anp_madmex)) # hacemos stack para convertir a df
+    anp_madmex_loss <- raster::stack(list(anp_loss, anp_madmex)) # hacemos stack para convertir a df
     
     # now you can convert this to a matrix or data.frame
-    anp_madmex_loss_df <- data.frame(anp = anp_shp, rasterToPoints(anp_madmex_loss)) %>% 
-        rename(year_loss = hansen_forest_loss_v1_3, clase_madmex = madmex_nalc_10c_30m_2010) %>% 
-        count(year_loss, clase_madmex) %>% 
-        mutate(anp = anp_shp) %>% 
+    anp_madmex_loss_df <- data.frame(anp = anp_shp, raster::rasterToPoints(anp_madmex_loss), 
+        stringsAsFactors = FALSE) %>% 
+        dplyr::rename(year_loss = hansen_forest_loss_v1_4_wgs84nodefs, 
+            clase_madmex = madmex_nalc_10c_30m_2010) %>% 
+        dplyr::count(year_loss, clase_madmex) %>% 
+        dplyr::mutate(anp = anp_shp) %>% 
         ungroup()
 }
 
 # parámetros en data.frame para la llamada de slurm_apply
-params_df <- dplyr::data_frame(anp_shp = anps_shp, path_anps_shp = path_anps_shp)
-sjob <- slurm_apply(perdida, params = params_df, jobname = "forest_loss_job", 
-    nodes = 5, cpus_per_node = 2, slurm_options = list(partition = "optimus", 
-        nodes = "5", ntasks = "3"), add_objects = c("hansen_loss", "madmex_class"))
+params_df <- data.frame(anp_shp = anps_shp, path_anps_shp = path_anps_shp, 
+    stringsAsFactors = FALSE)
+sjob <- slurm_apply(perdida, params = params_df, jobname = "forest_loss_job_rings_2", 
+    nodes = 4, cpus_per_node = 2, slurm_options = list(partition = "optimus", 
+        nodes = "4", ntasks = "4"), add_objects = c("hansen_loss", "madmex_class"))
 print_job_status(sjob)
 
 ### Read results
+setwd("/LUSTRE/sacmod/reportes_anp/procesar_forest_loss/_rslurm_forest_loss_job_2/")
+setwd("/LUSTRE/sacmod/reportes_anp/procesar_forest_loss/_rslurm_forest_loss_job_rings_2/")
+
 anp_loss_list <- list.files(".", "results", 
     full.names = TRUE) %>% 
     map(readRDS) %>% 
     flatten()
 
-# esto no hará falta con la nueva función, se conserva el orden??!!!
-anp_loss_list_named <- anp_loss_list %>% map2(anps_shp, 
-    function(x, y){
-        x$anp <- y
-        x
-    })
+perdida_anps_df <- bind_rows(anp_loss_list)
+perdida_anps_rings_df <- bind_rows(anp_loss_list)
 
-perdida_anps_df <- bind_rows(anp_loss_list_named)
-
-save(perdida_anps_df, file = "../../datos/2017-10-02_perdida_cobertura.RData")
-
-
-##### Correr en los anillos
-setwd("/LUSTRE/sacmod/reportes_anp/procesar_forest_loss")
-
-path_anps_rings_shp <- "/LUSTRE/MADMEX/eodata/footprints/anp/anp_rings"
-path_anps_shp <- "/LUSTRE/MADMEX/eodata/footprints/anp/anp_sinBuffer"
-anps_shp <- list.files(path_anps_shp, 
-    pattern = ".shp", recursive = FALSE) %>% 
-    tools::file_path_sans_ext()
-hansen_loss <- raster("/LUSTRE/sacmod/reportes_anp/datos/hansen_forest_loss_v1_3.tif")
-madmex_class <- raster("/LUSTRE/sacmod/reportes_anp/datos_procesados/madmex_nalc_10c_30m_2010.tif")
-
-params_df <- dplyr::data_frame(anp_shp = stringr::str_c(anps_shp, "_ring"), 
-    path_anps_shp = path_anps_rings_shp)
-sjob <- slurm_apply(perdida, params = params_df, jobname = "forest_loss_rings_job", 
-    nodes = 5, cpus_per_node = 2, slurm_options = list(partition = "optimus", 
-        nodes = "5", ntasks = "3"), add_objects = c("hansen_loss", "madmex_class"))
-
-##### Leer resultados
-library(purrr)
-library(tidyverse)
-library(stringr)
-library(plotly)
-
-setwd("/LUSTRE/sacmod/reportes_anp/procesar_forest_loss/_rslurm_forest_loss_rings_job/")
-
-anp_loss__rings_list <- list.files(".", "results", 
-    full.names = TRUE) %>% 
-    map(readRDS) %>% 
-    flatten()
-
-perdida_anps_rings_df <- bind_rows(anp_loss__rings_list)
-
-save(perdida_anps_rings_df, file = "../../datos/2017-10-04_perdida_cobertura_rings.RData")
-
+save(perdida_anps_df, file = "../../datos_procesados/2017-10-22_perdida_cobertura.RData")
+save(perdida_anps_rings_df, file = "../../datos_procesados/2017-10-22_perdida_cobertura_rings.RData")
