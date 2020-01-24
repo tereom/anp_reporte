3ae69d6da8b1d0ac0cdb5917fd99fb03e237c4f8
tereom <teresa.ortiz.mancera@gmail.com>
2020-01-24 17:16:38 -0600

    Arreglar para zonas de preservación

--- preprocesamiento/crear_tablas_ie_adi_anp.R
+++ preprocesamiento/crear_tablas_ie_adi_anp.R
@@ -26,6 +26,19 @@ calcular_ie <- function(anp_shp){
         valores = round(sample(anp_ie_v, min(length(anp_ie_v), 1000)), 3))
 }
 
+calcular_ie_zp <- function(anp_shp){
+    anp  <- st_read(anp_shp, stringsAsFactors = FALSE)
+    anp_ie <- ie_longlat %>% 
+        crop(anp) %>%
+        mask(mask = anp)
+    anp_ie_v <- na.omit(values(anp_ie))
+    anp_ie_v <- anp_ie_v[anp_ie_v >= 0]
+    list(anp = tools::file_path_sans_ext(basename(anp_shp)), 
+        media = mean(anp_ie_v), mediana = median(anp_ie_v), 
+        desv.est = sd(anp_ie_v), 
+        valores = round(sample(anp_ie_v, min(length(anp_ie_v), 1000)), 3))
+}
+
 # anps
 path_anps_shp <- "datos_insumo/shapes_anp/anp_sinBuffer"
 anps_shp <- list.files(path_anps_shp, pattern = ".shp", recursive = FALSE, 
@@ -44,11 +57,11 @@ anps_shp <- list.files(path_anps_shp, pattern = ".shp", recursive = FALSE,
     full.names = TRUE)
 ie_zn_list <- purrr::map(anps_shp, calcular_ie)
 
-# zonas preservación
+# zonas preservación (misma función sin distinct)
 path_anps_shp <- "datos_insumo/shapes_anp/anp_zonaspreservacion/"
 anps_shp <- list.files(path_anps_shp, pattern = ".shp", recursive = FALSE, 
     full.names = TRUE)
-ie_zp_list <- purrr::map(anps_shp, calcular_ie)
+ie_zp_list <- purrr::map(anps_shp, calcular_ie_zp)
 
 save(ie_list, ie_anillos_list, ie_zn_list, ie_zp_list, 
     file =  "datos_procesados/2018-09-04.RData")
